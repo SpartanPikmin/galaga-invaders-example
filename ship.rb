4d3276c5378c746a9e7ba74e418375eb5204e8fe
Devon Crane <cranede@10-116-14-109.wireless.oregonstate.edu>
2019-02-13 11:04:02 -0800

    In class Ex 11

--- ship.rb
+++ ship.rb
@@ -7,39 +7,45 @@ class Ship
 
   WIDTH = 100
   HEIGHT = 200
+  MOVE_5_UNITS = 5
+  MISSILE_MOVE_10_UNITS
 
   def initialize(x, y)
     @x = x
     @y = y
-    @missile_coordinates = []
+    @missile_coordinates = struct.new()
   end
 
   def move_left
-    @x -= 5
+    @x -= MOVE_5_UNITS
   end
 
   def move_right
-    @x += 5
+    @x += MOVE_5_UNITS
   end
 
-  def fire
-    @missile_coordinates << [@x, @y - HEIGHT / 2]
+  def fire_missile
+    @missile_coordinates << [@x, coordinate_center]
+  end
+  
+  def coordinate_center
+    (@y - HEIGHT / 2)
   end
 
   def move_missiles
     @missile_coordinates.each do |missile|
-      missile[1] -= 10
+      missile[1] -= MISSILE_MOVE_10_UNITS
     end
   end
 
   def draw
     # Ignore this
   end
-
+  
   def draw_missiles
-    @missile_coordinates.each do |missile|
+   @missile_coordinates.each do |missile|
 
-    end
+   end
   end
 
 end
@@ -48,9 +54,14 @@ end
 =begin
 Reflecting on Principle
 -----------------------
-1. What does the OOD promise to give us, as programmers?
-2. What does Martin's SOLID acronym stand for? (Just expand the acronym.)
-3. True or False: It is our professional responsibility to always spend time designing before writing our code (designing now rather than later).
-4. In Metz' TRUE acronym, what does the E stand for and what does it mean?
-5. How does applying SRP naturally lead to good design?
+1. What does the OOD promise to give us, as programmers? 
+    It makes it easier to modify and change the code in the future
+2. What does Martin's SOLID acronym stand for? (Just expand the acronym.) 
+    Single responsibility, Open-closed, Liskov subsistition, Interface segeregation, Dependency inversion
+3. True or False: It is our professional responsibility to always spend time designing before writing our code (designing now rather than later). 
+    False
+4. In Metz' TRUE acronym, what does the E stand for and what does it mean? 
+    Exemplary: encurage change using these qualities
+5. How does applying SRP naturally lead to good design? 
+    it makes it more moduler and eaisier to change in the future.
 =end
